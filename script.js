db8db153b7007322b3797fee3528b54e59958536
VitaliyTri <111489824+VitaliyTri@users.noreply.github.com>
2023-01-31 18:33:32 +0700

    first safe

---
+++ script.js
@@ -0,0 +1,17 @@
+// Задание 1
+
+let celcius = -11;
+console.log(`${celcius}°C`);
+
+let fahrenheit = (9 / 5) * celcius + 32;
+console.log(`${fahrenheit}°F`);
+
+console.log(`Температура ${celcius}°C равна температуре ${fahrenheit}°F`);
+
+////////////////////////////////////////////////////////////////////////////
+
+// Задание 2
+
+let name = 'Vitaliy';
+let admin = name;
+console.log(admin);
